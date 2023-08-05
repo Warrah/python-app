57a494fb6d498b957e64644cc399cf1e64402e7b
warrah_b <behramwarrah@gmail.com>
2023-08-05 17:19:41 +0500

    new

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hellooooooooo!!!</h1>"
+    return "<h1 style='color:purple'>Hellooooooooo from python!!!</h1>"
 
 if __name__ == "__main__":
     app.run(host='localhost')
